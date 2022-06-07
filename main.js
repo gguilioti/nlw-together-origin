c54dbeb29adb50813ebe2c4a77c80da4c5aeba2f
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-06-07 15:55:07 -0300

    aula05

--- main.js
+++ main.js
@@ -39,7 +39,13 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+      767: {
+        slidesPerView: 2,
+        serWrapperSize: true
+      }
+    }
   })
 
 
@@ -72,3 +78,36 @@ window.addEventListener('scroll', function(){
     backToTopButton.classList.remove('show')
   }
 })
+
+
+const sections = document.querySelectorAll('main section[id]')
+window.addEventListener('scroll', function() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for(const section of sections)
+  {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >=sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd)
+    {
+      document
+      .querySelector('nav ul li a[href*='+ sectionId +']')
+      .classList.add('active')
+    }
+    else
+    {
+      document
+      .querySelector('nav ul li a[href*='+ sectionId +']')
+      .classList.remove('active')
+    }
+  }
+})
+function activateMenuAtCurentSection() 
+{
+
+}
